1e4a9ed2da867b72c6fb1e9b9d7bbd673507abe6
Ahmadsaniliman <saniahmadliman@gmail.com>
2021-05-18 10:42:20 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var a;
+a = 5;
+var d;
+d = 10;
+console.log(a);
+
+var b = 'longson is my boy';
+console.log(b);
+
+c = 90;
+console.log(c);
+var c;
+
+
+function add(){
+    var e = 20;
+    var f = 30;
+}
+
+function sub(){
+    // console.log(e - f);
+}
+
+add();
+sub();
