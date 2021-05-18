b2110eb02b0d5d3354cb520125ac4ba01f437457
Ahmadsaniliman <saniahmadliman@gmail.com>
2021-05-18 15:18:55 +0100

    third commit

---
+++ app.js
@@ -0,0 +1,22 @@
+let a;
+let b;
+let c;
+let val;
+
+a = 'longson';
+console.log(a);
+console.log(typeof(a));
+
+b = 90;
+console.log(b);
+console.log(typeof(b));
+
+c = 90.01;
+console.log(c);
+console.log(typeof(c));
+
+a  = 10; 
+b = 90;
+c = a > b;
+console.log(c);
+console.log(typeof(c));
