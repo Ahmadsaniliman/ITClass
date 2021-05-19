3b71784161fa5421e4f15a0d98791cbaacc85ffa
Ahmadsaniliman <saniahmadliman@gmail.com>
2021-05-19 12:26:44 +0100

    third commit

---
+++ class_1.js
@@ -0,0 +1,11 @@
+// console.log(5 == 5);
+var age = 11;
+var gender = 'm';
+
+if(age >= 18 && gender == 'm'){
+    console.log("you can play the game since you are " + age + " year old")
+} else if (age < 18 && age > 10){
+    console.log('you can play pes4')
+} else {
+    console.log('get out go home and play ')
+}
