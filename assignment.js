0b35bdd635571372f987bd585256f860b537f7c8
Ahmadsaniliman <saniahmadliman@gmail.com>
2021-05-20 09:24:43 +0100

    fifth commit

---
+++ assignment.js
@@ -0,0 +1,25 @@
+// var a;
+// var b;
+// a = 70;
+// b = 601;
+
+// if( a > b){
+//     console.log(`${a} is the bigger number than ${b}`);
+// } else {
+//     b > a;
+//     console.log(`${b} is the bigger number than ${a}`);
+// };
+
+let age_1 = 0;
+let age_2 = 0;
+
+if(age_1 && age_2 >= 18){
+    console.log('you can be addmmited into the university');
+} else if(age_1 >= 18 && age_2 < 18){
+    console.log('age_1 can be addmited while age_2 is too young');
+} else if(age_1 < 18 && age_2 >= 18){
+    console.log('age_1 cannot be addmited while age_2 can be addmitted');
+} else {
+    age_1 && age_2 < 18;
+        console.log('Both cannot be addmited cos the are too young');
+};
