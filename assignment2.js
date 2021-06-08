b7bd24b6345eeda816d7bf2d5192968378b0194b
Ahmadsaniliman <saniahmadliman@gmail.com>
2021-06-08 08:06:58 +0100

    js assignment

---
+++ assignment2.js
@@ -0,0 +1,82 @@
+var students = [
+    {
+        id : 1,
+        fName : 'longso',
+        surname : 'simpson',
+        address : 'federal lowcost',
+        age : 29,
+        phone : 0808554698,
+    },
+    {
+        id : 2,
+        fName : 'zabson',
+        surname : 'zabiyya',
+        address : 'filin sukuwa',
+        age : 18,
+        phone : 0907646893,
+    },
+    {
+        id : 3,
+        fName : 'Abdulladdi',
+        surname : 'Allahnana',
+        address : 'filin sukuwa',
+        age : 39,
+        phone : 0808765780,
+    },
+    {
+        id : 4,
+        fName : 'daveSon',
+        surname : 'dave',
+        address : 'ix note',
+        age : 78,
+        phone : 090876586,
+        },
+        {
+            id : 5,
+            fName : 'kunle',
+            surname : 'arawosegbe',
+            address : 'tundun wada',
+            age : 23,
+
+        },
+        {
+            id : 6,
+            fName : 'favour',
+            surname : 'Didan',
+            address : 'ring road',
+            age : 18,
+        },
+        {
+            id : 7,
+            fName : 'femi',
+            surname : 'joseph',
+            age : 25,
+            address : 'west of mines',
+        },
+        {
+            id : 8,
+            fName : 'weng',
+            surname : 'ibrahim',
+            address : 'anguwa rukuba',
+            age: 24,
+        },
+        {
+            id : 9,
+            fName :'chioma',
+            surname : 'ekwe',
+            age : 23,
+            address : 'rantiya',
+        },
+        {
+            id : 10,
+            fName : 'manasseh',
+            surname : 'andy',
+            age : 27,
+            adress : 'mina',
+        },
+]; 
+
+// for(let student of students){
+//     console.log(Array(student.fName));
+// };
+// console.log(students.filter((student) => student.id == 1));
